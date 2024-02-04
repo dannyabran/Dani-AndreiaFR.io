6815d321fc366d503cedcaca20ca514a96951501
dannyabran <danielgarciaabrantes30@gmail.com>
2024-02-04 20:23:48 +0000

    data

--- typeit.js
+++ typeit.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 new TypeIt("#element", {
-    strings: ["On va se marier!", "Papa el mama ne font rien dans l'ordre!!", "Ils m'ont eu, el puis ils décident de me faire baptiser!! Ah désolé, je me suis pas présenté :)", "Je m'appelle Lorenzo le plus petit de la famille...", "Et bien moi je vais reprendre les rennes de leur vie et le jour de mon baptême, j'ai décidé qu'ils se marieraient aussi", "Annulez tout ce que vous avez de prévu et rejoignez-nous pour celle journée spéciale..."],
+    strings: ["On va se marier!", "Papa el mama ne font rien dans l'ordre!!", "Ils m'ont eu, el puis ils décident de me faire baptiser!! Ah désolé, je me suis pas présenté :)", "Je m'appelle Lorenzo le plus petit de la famille...", "Et bien moi je vais reprendre les rennes de leur vie et le jour de mon baptême, j'ai décidé qu'ils se marieraient aussi", "Annulez tout ce que vous avez de prévu et rejoignez-nous pour celle journée spéciale...", "Rendez-Vous le 17 Août de 2024"],
     speed: 50,
     cursor: false,
     loop: false,
